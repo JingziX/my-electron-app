9da79384508b322b57301f9a052b6a0cb1bcfce3
jingzi <1163478123@qq.com>
2024-06-26 16:10:37 +0800

    增肌字体图标，计算方式变更

--- static/add/index.js
+++ static/add/index.js
@@ -1,20 +1,44 @@
+/*
+ * @Author: jingzi 1163478123@qq.com
+ * @Date: 2023-10-18 18:04:15
+ * @LastEditors: jingzi 1163478123@qq.com
+ * @LastEditTime: 2024-06-26 15:45:46
+ * @Description: 文件求和
+ * Copyright (c) 2024 by ${git_name}, All Rights Reserved.
+ */
 const fs = require('fs');
 const { dialog } = require('electron')
 window.onload = ()=>{
     const openDialogBtn = document.querySelector('#openDialogBtn')
-    const myFile = document.querySelector('#myFile')
-    const total = document.querySelector('#total')
-    const numDom = document.querySelector('#nums')
+    const myFile = document.querySelector('#myFile') // 选中的文件
+    const total = document.querySelector('#total') // 文件计算总和
+    const numDom = document.querySelector('#nums') // 所有文件名
+    const historyTotal = document.querySelector('#historyTotal') // 历史记录
+    const addHistory = document.querySelector('#addHistory') // 计算历史记录总和
+    const clearHistory = document.querySelector('#clearHistory') // 清除历史记录
+    let history = []
     openDialogBtn.addEventListener('click',()=>{
         const nums = []
         for (let index = 0; index < myFile.files.length; index++) {
             const ele = myFile.files[index];
             const name = ele.name.replace(/\.(\w+)$/g,'')
-            const data = name.replace(/\-|\_/g,'')
-            nums.push(+data)
+            if(name.indexOf('行程')===-1){
+                const data = name.replace(/\-|\_/g,'')
+                nums.push(+data)
+            }
         }
         const sum = nums.reduce((total, item) => total + item, 0)
+        history.push(sum)
         total.innerHTML=`总和为：${sum}；\n 总个数：${nums.length}`
         numDom.innerHTML = `所有文件为：${nums.join(', ')}`
+        historyTotal.innerHTML = `所有历史数据数据：${history.join(', ')}`
+    })
+    addHistory.addEventListener('click',()=>{
+        const hTotal = history.reduce((prev,cur)=> prev+cur)
+        historyTotal.innerHTML = `所有数据：${history.join(', ')}，历史数据总和为：${hTotal}`
+    })
+    clearHistory.addEventListener('click',()=>{
+        historyTotal.innerHTML = ``
+        history=[]
     })
 }
